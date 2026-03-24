30750cb8bc4fa46d7834af29a9c12d78534312e2
Academy Student <academy.student@lafosse.com>
2026-03-24 22:11:20 +0000

    Added testing code in gameselection-test.js

--- client/assets/gameselection-test.js
+++ client/assets/gameselection-test.js
@@ -1,9 +1,29 @@
-describe ("Quiz Navigation"), () => {
+describe ("gameselection.html"), () => {
+// Look at the start game button
+it("has a start button", () => {
+    const btn = document.querySelector("btn-primary")
+    expect(btn).toBeTruthy()
+    expect(btn.innerHTML).toContain("Start Game")
+  })
+
+  it("question is empty when website load", () => {
+    const h3 = document.querySelector("h3");
+    expect(h3).toBeTruthy()
+    expect(h3.innerHTML).toBe("")
+  })
+
+  it("displays the first question when the start button is clicked", () => {
+    const btn = document.querySelector("btn-primary")
+    btn.click();
+    const h3 = document.querySelector('h3')
+    expect(h3.innerHTML).toBe("question 1")
+  })
+
+  
+  it("displays the next question when the next button is clicked", () => {
+    const btn = document.querySelector("button")
+    expect(btn.innerHTML).toBe("")
+  })
 
 }
 
-//has a button
-//trigger next question
-//display the question 
-//text should display the next question
-// 
