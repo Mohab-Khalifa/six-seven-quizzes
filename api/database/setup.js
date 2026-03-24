3ce2de83700e1e4ffa8409607f51c4f87e6dfccf
mohab-khalifa <khalifamohab97@outlook.com>
2026-03-24 11:55:36 +0000

    created connection to db

---
+++ api/database/setup.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const fs = require('fs');
+
+const db = require('./connect');
+
+const sql = fs.readFileSync('./database/setup.sql').toString();
+
+db.query(sql)
+    .then(() => {
+        db.end();
+        console.log("Set-up complete.")
+    })
+    .catch(error => console.log(error));
