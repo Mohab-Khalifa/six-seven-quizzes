4194ae7ec9069cac410465a80f5829db6ddcbe5c
papaparadox <tymurholovko@gmail.com>
2026-03-24 17:26:15 +0000

    Login page now correctly checks the password from the database and redirects user to the gameselection.html page

---
+++ client/assets/scripts/login.js
@@ -0,0 +1,27 @@
+document.getElementById("form-box").addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const form = new FormData(e.target);
+
+  const options = {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: form.get("username"),
+      password: form.get("password"),
+    }),
+  };
+
+  const response = await fetch("http://localhost:3012/login", options);
+  const data = await response.json();
+
+  if (response.status == 200) {
+    localStorage.setItem("token", data.token);
+    window.location.assign("HomePage.html");
+  } else {
+    alert(data.error);
+  }
+});
