093e9edb389998754040968b9bce32975554e659
papaparadox <tymurholovko@gmail.com>
2026-03-24 14:14:18 +0000

    Changed the form for signing up and added more functionality to the DOM as the user clicks register. It now sends the request to a fictional endpoint

--- client/assets/scripts/signup.js
+++ client/assets/scripts/signup.js
@@ -1,32 +1,27 @@
-document
-  .getElementById("register-form")
-  .addEventListener("submit", async (e) => {
-    e.preventDefault();
+document.getElementById("form-box").addEventListener("submit", async (e) => {
+  e.preventDefault();
 
-    const form = new FormData(e.target);
+  const form = new FormData(e.target);
 
-    const options = {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        username: form.get("username"),
-        password: form.get("password"),
-      }),
-    };
+  const options = {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: form.get("username"),
+      password: form.get("password"),
+    }),
+  };
 
-    const response = await fetch(
-      "http://localhost:3000/users/register",
-      options,
-    );
-    const data = await response.json();
+  const response = await fetch("http://localhost:3000/users/register", options);
+  const data = await response.json();
 
-    if (response.status == 201) {
-      alert("Registered!");
-      window.location.assign("login.html");
-    } else {
-      alert(data.error);
-    }
-  });
+  if (response.status == 201) {
+    alert("Registered!");
+    window.location.assign("login.html");
+  } else {
+    alert(data.error);
+  }
+});
